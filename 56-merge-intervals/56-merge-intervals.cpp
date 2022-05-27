a380f2112f67580a15046d512c278bd7983e3a9e
Abhijit Gupta <59078878+gupabhi100@users.noreply.github.com>
2022-05-27 17:17:41 +0545

    Time: 77 ms (19.92%), Space: 19.1 MB (67.56%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -5,7 +5,7 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> output;
         output.push_back(intervals[0]);
-        for(int i=1; i<intervals.size(); i++) {
+        for(int i=0; i<intervals.size(); i++) {
             if(output.back()[1] >= intervals[i][0]) output.back()[1] = max(output.back()[1] , intervals[i][1]);
             else output.push_back(intervals[i]); 
         }
